70c74bf2698b8603a5dc27ca5b35f5ea2e127e98
ManojKumaaar <lmanojkum99@gmail.com>
2025-05-24 23:59:32 +0000

    Intial Commit

---
+++ src/main/java/com/forecasting/ai/controller/ForecastingController.java
@@ -0,0 +1,38 @@
+package com.forecasting.ai.controller;
+
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.*;
+
+import com.forecasting.ai.service.GeminiService;
+import com.forecasting.ai.request.ForecastRequest;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class ForecastingController {
+
+    @Autowired
+    private GeminiService geminiService;
+
+    String queryNul="";
+    @GetMapping("/chatAI")
+    public String searchAI(@RequestParam String user ,@RequestParam String query) throws Exception {
+        return geminiService.getForecast(false, query, user);
+    }
+
+    @GetMapping("/getForecast")
+    public String generateForecast(@RequestParam String user) throws Exception {
+        return geminiService.getForecast(false, queryNul, user);
+    }
+//    @Scheduled(cron = "0 0 0 25 * ?")
+    @Scheduled(fixedRate = 200000)
+    public void scheduledForecast() throws Exception {
+        geminiService.getForecast(true, queryNul, null);
+    }
+}
+
